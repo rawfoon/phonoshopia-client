9cf8cf61024b95dc949eb0123cce94ccdb0a11bc
rawfoon <mgrawfoon@gmail.com>
2022-11-25 11:39:30 +0600

    routes and layout created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './Routes/Routes/Route';
 
 function App() {
   return (
     <div >
 
-<button className="btn glass">Glass button</button>
-
-<button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+<RouterProvider router={routes}></RouterProvider>
       
     </div>
   );
