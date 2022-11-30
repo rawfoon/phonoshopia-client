be63f7460148e22c1e16344ef863dedd403a5b8a
rawfoon <mgrawfoon@gmail.com>
2022-11-30 23:34:56 +0600

    readme.md edited and add tow more section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import useTitle from '../../../hooks/useTitle';
 import Advertisment from '../../Advertisment/Advertisment';
+import OffSection from '../../Advertisment/OffSection/OffSection';
 import Categories from '../../Categories/Categories/Categories';
 import Banner from '../Banner/Banner';
 import DemoSection from '../DemoSection/DemoSection';
+import Subscribe from '../Subscribe/Subscribe';
 
 const Home = () => {
+    useTitle('Home')
     return (
         <div>
             {/* <h3>This is home</h3> */}
             <Banner></Banner>
             <Categories></Categories>
+            <OffSection></OffSection>
             <Advertisment></Advertisment>
             <DemoSection></DemoSection>
+            <Subscribe></Subscribe>
+            
 
         </div>
     );
